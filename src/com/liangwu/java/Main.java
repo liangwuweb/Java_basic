232054b72eb85b2c488e5c19071f98e6736c58a1
Liang Wu <liangwuweb@gmail.com>
2022-09-01 21:47:00 -0500

    HelloWrold with Java

---
+++ src/com/liangwu/java/Main.java
@@ -0,0 +1,11 @@
+package com.liangwu.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    System.out.println("Hello from IDEA");
+    String firstName = "Liang";
+    System.out.println("My first name is " + firstName);
+    }
+}

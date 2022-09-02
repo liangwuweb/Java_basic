a93a6b89408bc2f3c936a0f95189d1cc347deeac
Liang Wu <liangwuweb@gmail.com>
2022-09-02 10:11:51 -0500

    Character

--- src/com/liangwu/java/Main.java
+++ src/com/liangwu/java/Main.java
@@ -5,23 +5,26 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-    boolean b1 = true;
-    boolean b2 = false;
+    char c1 = '1';
+    char c2 = '2';
+    char c3 = '3';
 
-    System.out.println("the value of b1 is: " + b1);
-    System.out.println("the value of b2 is: " + b2);
-        System.out.println("the value of def is: " + def);
+    System.out.println("Char 1: " + c1);
+    System.out.println("Char 2: " + c2);
+    System.out.println("Char 3: " + c3);
 
-    boolean b3 = !b1;
-        System.out.println("the value of b3 is: " + b3);
+    char dollarSign = '\u0024';
+    System.out.println("Dollar Sign: " + dollarSign);
 
-    int x = 0;
-    boolean b4= (x == 0);
-        System.out.println("the value of b4 is: " + b4);
+    char a = 'a';
+    char b = 'b';
+    char c= 'c';
+    System.out.print(Character.toUpperCase(a));
+    System.out.print(Character.toUpperCase(b));
+    System.out.println(Character.toUpperCase(c));
+    System.out.print(Character.toUpperCase(c));
 
-    String sBoolean = "true";
-    boolean parsed = Boolean.parseBoolean(sBoolean);
-        System.out.println("the value of parsed is: " + parsed);
+
 
 
 

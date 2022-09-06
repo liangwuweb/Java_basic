9e01b2ae5368a90e33681d0d8c08f4fcde9baee1
Liang Wu <liangwuweb@gmail.com>
2022-09-06 10:45:43 -0500

    build a string from multiple values

--- src/com/liangwu/java/Main.java
+++ src/com/liangwu/java/Main.java
@@ -1,22 +1,37 @@
 package com.liangwu.java;
 import java.text.NumberFormat;
+import java.util.Scanner;
 
 public class Main {
     static boolean def;
 
     public static void main(String[] args) {
 	    // write your code here
-        int intValue = 42;
-        String fromInt = Integer.toString(intValue);
-        System.out.println(fromInt);
+        String str1 = "Hello";
+        String str2 = "World";
+        String str3 = str1 + ", " + str2 + "!";
+        System.out.println(str3);
 
-        boolean boolValue = true;
-        String fromBool = Boolean.toString(boolValue);
-        System.out.println(fromBool);
+        StringBuilder sb = new StringBuilder("Hello")
+                .append(", ")
+                .append("World")
+                .append("!");
+        System.out.println(sb);
 
-        long longValue = 10_000_000;
-        NumberFormat formatter = NumberFormat.getNumberInstance();
-        String formatted = formatter.format(longValue);
-        System.out.println(formatted);
+        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Enter Value: ");
+//        String input = scanner.nextLine();
+//        System.out.println(input);
+
+        // Delete sb
+        sb.delete(0, sb.length());
+        System.out.println(sb);
+
+        for(int i = 0; i < 3; i++) {
+            String input = scanner.nextLine();
+            sb.append(input + "\n");
+        }
+
+        System.out.println(sb);
     }
 }

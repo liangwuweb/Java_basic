426f328ed73ff65058fb8a2da9fdc45972abd539
Liang Wu <liangwuweb@gmail.com>
2022-09-06 12:06:32 -0500

    parse string values

--- src/com/liangwu/java/Main.java
+++ src/com/liangwu/java/Main.java
@@ -4,24 +4,18 @@ import java.util.Scanner;
 import java.util.Locale;
 
 public class Main {
-    static boolean def;
 
     public static void main(String[] args) {
-        Locale locale = new Locale("da", "DK");
+        String s1 = "Welcome to Wisconsin!";
+        System.out.println("Length of string: " + s1.length());
 
-        double doubleValue = 1_234_567.89;
+        int position = s1.indexOf("Wisconsin");
+        System.out.println("Position of substring: " + position);
 
-        NumberFormat numF = NumberFormat.getNumberInstance(locale);
-        System.out.println("Number: " + numF.format(doubleValue));
-
-        NumberFormat curF = NumberFormat.getCurrencyInstance(locale);
-        System.out.println("Currency: " + curF.format(doubleValue));
-
-        NumberFormat intF = NumberFormat.getIntegerInstance();
-        System.out.println("Interger: " + intF.format(doubleValue));
-
-
-
-
+        String s2 = "welcome! ";
+        int len1 = s2.length();
+        System.out.println(len1);
+        String s3 = s2.trim();
+        System.out.println(s3.length());
     }
 }

026678ff30486426e813fe9ac26c5719a5c297dc
Liang Wu <liangwuweb@gmail.com>
2022-09-06 11:49:26 -0500

    Format numeric values as strings

--- src/com/liangwu/java/Main.java
+++ src/com/liangwu/java/Main.java
@@ -1,29 +1,27 @@
 package com.liangwu.java;
 import java.text.NumberFormat;
 import java.util.Scanner;
+import java.util.Locale;
 
 public class Main {
     static boolean def;
 
     public static void main(String[] args) {
-        String str1 = "Hello";
-        String str2 = "hello";
+        Locale locale = new Locale("da", "DK");
 
-        if (str1 == str2) {
-            System.out.println("They match");
-        } else {
-            System.out.println("They don't match");
-        }
+        double doubleValue = 1_234_567.89;
 
-        String part1 = "Hello ";
-        String part2 = "World";
-        String str4 = part1 + part2;
-        String str5 = "Hello World";
-        if (str4.equals(str5)) {
-            System.out.println("match");
-        } else {
-            System.out.println("no match");
-        }
+        NumberFormat numF = NumberFormat.getNumberInstance(locale);
+        System.out.println("Number: " + numF.format(doubleValue));
+
+        NumberFormat curF = NumberFormat.getCurrencyInstance(locale);
+        System.out.println("Currency: " + curF.format(doubleValue));
+
+        NumberFormat intF = NumberFormat.getIntegerInstance();
+        System.out.println("Interger: " + intF.format(doubleValue));
+
+
+
 
     }
 }

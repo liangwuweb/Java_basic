2b56c1e9f367166b7c5f21aa0b26b39c9ecf1e48
Liang Wu <liangwuweb@gmail.com>
2022-09-06 16:17:40 -0500

    Debbugging with InterlliJ IDEA

--- src/com/liangwu/java/Main.java
+++ src/com/liangwu/java/Main.java
@@ -6,19 +6,11 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a numeric Value: ");
-        String input1 = scanner.nextLine();
-        System.out.print("Enter a numeric Value: ");
-        String input2 = scanner.nextLine();
+        String welcome = "Welcome!";
+        char[] chars = welcome.toCharArray();
 
-        int a = Integer.parseInt(input1);
-        int b = Integer.parseInt(input2);
-
-        int sum = a + b;
-        System.out.println("Sum is: " + sum);
-
-
-
+        for (int i = 0; i < chars.length; i++) {
+            System.out.println(chars[i]);
+        }
     }
 }

607b7bd9c40b7ecc076f2310636d0107f1f62884
Liang Wu <liangwuweb@gmail.com>
2022-09-08 11:32:19 -0500

    Overloading method with different signitures

--- src/com/liangwu/java/Main.java
+++ src/com/liangwu/java/Main.java
@@ -7,21 +7,50 @@ public class Main {
     static String[] months = {"Jan", "Feb", "Mar", "April", "May", "June",
             "July", "August", "Sep", "Oct", "Nov", "Dec"};
     public static void main(String[] args) {
-        loopIt("Month of the year");
-        loopIt("Second run");
+       String s1 = getInput("Enter value 1: ");
+       String s2 = getInput("Enter value 2: ");
+       String s3 = getInput("Enter value 3: ");
+
+       double result = addValues(s1, s2);
+       System.out.println("The anwser is: " + result);
+
+        double result2 = addValues(s1, s2, s3);
+        System.out.println("The anwser of 3 values is: " + result2);
+
+        double result3 = addValues(s1, s2, s3, s1, s2, s3);
+        System.out.println("The anwser of many values is: " + result3);
     }
 
-    static void loopIt(String label) {
-        System.out.println(label);
+    static String getInput(String prompt) {
+        System.out.print(prompt);
+        Scanner sc = new Scanner(System.in);
 
-        for (int i = 0; i < label.length(); i++) {
-            System.out.print("#");
-        }
-
-        System.out.println("");
-
-        for (int i = 0; i < months.length; i++) {
-            System.out.println(months[i]);
-        }
+        return sc.nextLine();
     }
+
+    static double addValues(String s1, String s2) {
+        double d1 = Double.parseDouble(s1);
+        double d2 = Double.parseDouble(s2);
+
+        return d1 + d2;
+    }
+
+    static double addValues(String s1, String s2, String s3) {
+        double d1 = Double.parseDouble(s1);
+        double d2 = Double.parseDouble(s2);
+        double d3 = Double.parseDouble(s3);
+
+        return d1 + d2 + d3;
+    }
+
+    static double addValues(String ... values) {
+        double result = 0;
+        for (String value: values) {
+            double d = Double.parseDouble(value);
+            result += d;
+        }
+        return result;
+    }
+
+
 }

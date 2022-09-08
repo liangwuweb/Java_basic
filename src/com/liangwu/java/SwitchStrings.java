82edb7d743ae0cc0c75e2b9b2a12c1e4e19d5a9c
Liang Wu <liangwuweb@gmail.com>
2022-09-07 23:03:11 -0500

    different loop

--- src/com/liangwu/java/SwitchStrings.java
+++ src/com/liangwu/java/SwitchStrings.java
@@ -6,23 +6,36 @@ import java.util.Scanner;
 public class SwitchStrings {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        String[] months = {"Jan", "Feb", "Mar", "April", "May", "June",
+                "July", "August", "Sep", "Oct", "Nov", "Dec"};
 
-        System.out.print("Enter a string: ");
-        String input = sc.nextLine();
+        // incremental loop
+//        for (int i = 0; i < months.length; i++) {
+//            System.out.println(months[i]);
+//        }
 
-        switch (input) {
-            case "Jan":
-                System.out.println("The month is Jan");
-                break;
-            case "Feb":
-                System.out.println("The month is Feb");
-                break;
-            case "Mar":
-                System.out.println("The month is March");
-                break;
-            default:
-                System.out.println("You choose another month");
-        }
+//        for (int i = months.length - 1; i >= 0; i--) {
+//            System.out.println(months[i]);
+//        }
+
+        //for each loop
+//        for (String month : months) {
+//            System.out.println(month);
+//        }
+
+        // While loop
+//        int counter = 0;
+//        while (counter < months.length) {
+//            System.out.println(months[counter]);
+//            counter++;
+//        }
+
+        // Do while
+        int counter = 0;
+        do {
+            System.out.println(months[counter]);
+            counter++;
+        } while (counter < months.length);
+
     }
 }
